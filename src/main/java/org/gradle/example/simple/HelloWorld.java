72b7f16cd00cceae2bdd9932db9c4dee6b06d04e
Dmitry Nikolaev <cameltheman@gmail.com>
2016-01-05 00:25:02 +0300

    Change spaces to tabs.

--- src/main/java/org/gradle/example/simple/HelloWorld.java
+++ src/main/java/org/gradle/example/simple/HelloWorld.java
@@ -1,7 +1,7 @@
 package org.gradle.example.simple;
 
 class HelloWorld {
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
-    }
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+	}
 }
